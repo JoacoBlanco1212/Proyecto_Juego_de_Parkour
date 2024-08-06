374943e528f88f7fcb53fdc5093cfefc28c4d616
Joaquin Blanco <48386218@est.ort.edu.ar>
2024-08-06 09:51:25 -0300

    Limited speed and added drag.

--- Assets/CharacterControllerScript.cs
+++ Assets/CharacterControllerScript.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class CharacterControllerScript : MonoBehaviour
 {
     [Header("Movement")]
+
     public float moveSpeed;
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("Ground check")]
+
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
 
     public Transform orientation;
-
     float horizontalInput;
     float verticalInput;
 
@@ -27,7 +39,20 @@ public class CharacterControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
         MyInput();
+        SpeedControl();
+        //Ground drag
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
     }
 
     private void FixedUpdate()
@@ -39,12 +64,32 @@ public class CharacterControllerScript : MonoBehaviour
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
     }
 
     private void MyInput()
     {
+        //Get keyboard inputs
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        //Limit speed if needed
+        if (flatVel.magnitude > moveSpeed) //if current vel is higher than limit vel
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed; // calculated limitedVel
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z); //applies limitedVel
+        }
+    }
+
+    private void Jump()
+    {
+        //Reset Y vel
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 }
