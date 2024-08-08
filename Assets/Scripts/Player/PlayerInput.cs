196ffc92ddf1c372af4a22438fab6192f6f778e3
schustertomi <163531821+SchusterTomi@users.noreply.github.com>
2024-08-08 12:07:50 -0300

    Settings Panel (8:02)

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public PauseMenu pauseMenu;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.Pause();
+        }
+    }
+}
---
+++ Assets/Scripts/UI Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using TMPro;
+public class MainMenu : MonoBehaviour
+{
+    public TMP_Dropdown graphicsDropDown;
+    public Slider masterVol, musicVol, sfxVol;
+    public AudioMixer mainAudioMixer;
+    public void ChangeGraphicsQuality()
+    {
+        QualitySettings.SetQualityLevel(graphicsDropDown.value);
+    }
+    public void ChangeMasterVolume()
+    {
+        mainAudioMixer.SetFloat("MasterVol", masterVol.value);
+    }
+    public void ChangeMusicVolume()
+    {
+        mainAudioMixer.SetFloat("MusicVol", masterVol.value);
+    }
+    public void ChangeSfxVolume()
+    {
+        mainAudioMixer.SetFloat("SfxVol", sfxVol.value);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
