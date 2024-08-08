5e05939e401c521a989c36d423df43c18a6ae16a
schustertomi <163531821+SchusterTomi@users.noreply.github.com>
2024-08-08 14:05:21 -0300

    MainMenu finished?

--- Assets/Scripts/UI Scripts/MainMenu.cs
+++ Assets/Scripts/UI Scripts/MainMenu.cs
@@ -7,29 +7,44 @@ using UnityEngine.Audio;
 using TMPro;
 public class MainMenu : MonoBehaviour
 {
+    // Parte de Graphics Quality
     public TMP_Dropdown graphicsDropDown;
+
+    // Parte de Volumen
     public Slider masterVol, musicVol, sfxVol;
     public AudioMixer mainAudioMixer;
+
+    // Cambio de Graficos
     public void ChangeGraphicsQuality()
     {
         QualitySettings.SetQualityLevel(graphicsDropDown.value);
     }
+
+    // Cambio de Volumen General
     public void ChangeMasterVolume()
     {
         mainAudioMixer.SetFloat("MasterVol", masterVol.value);
     }
+
+    // Cambio de Volumen de música
     public void ChangeMusicVolume()
     {
-        mainAudioMixer.SetFloat("MusicVol", masterVol.value);
+        mainAudioMixer.SetFloat("MusicVol", musicVol.value);
     }
+
+    // Cambio de Volumen de los "efectos especiales" (Sfx)
     public void ChangeSfxVolume()
     {
         mainAudioMixer.SetFloat("SfxVol", sfxVol.value);
     }
+
+    // Cambio de escena a Game (escena)
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
     }
+
+    // Funcionamiento de cerrar juego
     public void QuitGame()
     {
         Application.Quit();
