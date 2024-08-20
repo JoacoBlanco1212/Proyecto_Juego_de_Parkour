9ee8ee6965b83cb6a3daa497480dde10a1ecf33e
schustertomi <163531821+SchusterTomi@users.noreply.github.com>
2024-08-20 11:12:06 -0300

    A lot of changes on scene(Game)

---
+++ Assets/Scripts/UI Scripts/OnLastCheckpointReached.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class OnLastCheckpointReached : MonoBehaviour
+{
+    public RaceTrigger raceTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "PlayerObj")
+        {
+            raceTrigger.OnLastCheckpointReached();
+        }
+    }
+}
---
+++ Assets/Scripts/UI Scripts/Race.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Race : MonoBehaviour
+{
+
+    [SerializeField] GameObject player;
+    [SerializeField] List<GameObject> checkPoints;
+    [SerializeField] Vector3 vectorPoint;
+    [SerializeField] float dead;
+
+    void Update()
+    {
+        if (player.transform.position.y < -dead)
+        {
+            player.transform.position = vectorPoint;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Checkpoint"))
+        {
+            vectorPoint = player.transform.position;
+            Checkpoint checkpoint = other.GetComponent<Checkpoint>();
+            checkpoint.ActivateNextCheckpoint();
+            Destroy(other.gameObject);
+        }
+       
+    }
+    
+}
