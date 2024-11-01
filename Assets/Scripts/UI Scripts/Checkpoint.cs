67726f7a96d970cac87245858ba0312695d334d1
schustertomi <163531821+SchusterTomi@users.noreply.github.com>
2024-11-01 10:46:35 -0300

    finished

--- Assets/Scripts/UI Scripts/Checkpoint.cs
+++ Assets/Scripts/UI Scripts/Checkpoint.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     public GameObject nextCheckpoint;
+    private Vector3 position;
+
+    private float time;
+
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        transform.position = new Vector3(position.x, position.y + Mathf.Cos(time) / 5, position.z);
+    }
+
     public void ActivateNextCheckpoint() // Activo el prox Checkpoint
     {
         if (nextCheckpoint != null)
